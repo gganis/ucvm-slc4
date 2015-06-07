7feda422f76a9c8d90fb308e9f244ccb20c1047f
Gerardo Ganis <gerardo.ganis@cern.ch>
2015-06-07 20:41:36 +0200

    Fix mounted path

--- etc/start-slc4.sh
+++ etc/start-slc4.sh
@@ -1,3 +1,3 @@
 # Run the relevant docker container 
-docker run -i -t -v /cvmfs/cernvm-slc4.cern.ch/cvm3:/cernvm -v /cvmfs/aleph.cern.ch:/cvmfs/aleph.cern.ch -v /home/ganis/local/MIT:/tmp/MIT -v /cvmfs/sft.cern.ch:/cvmfs/sft.cern.ch ucvm-slc4-docker /init
+docker run -i -t -v /cvmfs/cernvm-slc4.cern.ch/cvm3:/cernvm -v /cvmfs/aleph.cern.ch:/cvmfs/aleph.cern.ch -v /home/ganis/local/MIT:/MIT -v /cvmfs/sft.cern.ch:/cvmfs/sft.cern.ch ucvm-slc4-docker /init
  
